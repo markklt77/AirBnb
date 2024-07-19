9e92399d0bed6c31aff7ce239d9c5219e1ad94b0
markklt77 <markklt77@gmail.com>
2024-07-19 00:55:35 -0700

    added firstName and lastName columns to user

---
+++ backend/db/migrations/20240719043541-add-firstName-lastName-columns-to-users.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn("Users", 'firstName', {
+      type: Sequelize.STRING(50),
+      allowNull: false,
+      defaultValue: ''
+    }, options)
+
+    await queryInterface.addColumn('Users', 'lastName', {
+      type: Sequelize.STRING(50),
+      allowNull: false,
+      defaultValue: ''
+    }, options)
+  },
+
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Users', 'firstName', options)
+    await queryInterface.removeColumn('Users', 'lastName', options)
+  }
+};
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -5,6 +5,7 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { UnknownConstraintError } = require('sequelize');
 
 const router = express.Router();
 
@@ -33,14 +34,16 @@ router.post(
     '/',
     validateSignup,
     async (req, res) => {
-      const { email, password, username } = req.body;
+      const { email, password, username, firstName, lastName } = req.body;
       const hashedPassword = bcrypt.hashSync(password);
-      const user = await User.create({ email, username, hashedPassword });
+      const user = await User.create({ email, username, hashedPassword, firstName, lastName });
 
       const safeUser = {
         id: user.id,
         email: user.email,
         username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName
       };
 
       await setTokenCookie(res, safeUser);
