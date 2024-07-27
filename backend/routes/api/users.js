53cc07078ac657d6792c8fe363b8d7172d6a0612
markklt77 <markklt77@gmail.com>
2024-07-27 04:19:45 -0700

    changed sign up user route to include first and last name

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -42,8 +42,8 @@ router.post(
         id: user.id,
         email: user.email,
         username: user.username,
-        // firstName: user.firstName,
-        // lastName: user.lastName
+        firstName: user.firstName,
+        lastName: user.lastName
       };
 
       await setTokenCookie(res, safeUser);
