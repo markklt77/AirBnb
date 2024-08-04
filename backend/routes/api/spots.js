d23285f29f18bf4e0529f9bc87bc1b80c98c9e7a
markklt77 <markklt77@gmail.com>
2024-08-03 18:40:09 -0700

    fixed booking error message

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -341,7 +341,7 @@ router.post('/:spotId/bookings', requireAuth, async(req, res, next) => {
             errors.startDate = 'Start date cannot be in the past';
         }
         if (eDateObj <= sDateObj) {
-            errors.endDate = 'End date must be after the start date';
+            errors.endDate = "endDate cannot be on or before startDate";
         }
         if (Object.keys(errors).length) {
             const error = new Error('Bad Request');
