ba59f98c554c36a6a54024025fc15abadcf30e47
markklt77 <markklt77@gmail.com>
2024-09-01 20:53:30 -0700

    completed frontend auth-me deployment phase

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,56 +1,43 @@
 // backend/routes/index.js
-const path = require('path');
 const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
 
-
 router.use('/api', apiRouter);
 
-// Add a XSRF-TOKEN cookie
-// router.get("/api/csrf/restore", (req, res) => {
-//     const csrfToken = req.csrfToken();
-//     res.cookie("XSRF-TOKEN", csrfToken);
-//     res.status(200).json({
-//       'XSRF-Token': csrfToken
-//     });
-//   });
-
-// Add a XSRF-TOKEN cookie in development
-if (process.env.NODE_ENV !== 'production') {
-  router.get('/api/csrf/restore', (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie('XSRF-TOKEN', csrfToken);
-    res.status(200).json({
-      'XSRF-Token': csrfToken
-    });
-  });
-}
-
-// Static routes for production
+// Static routes
+// Serve React build files in production
 if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
   // Serve the frontend's index.html file at the root route
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
     );
   });
 
   // Serve the static assets in the frontend's build folder
-  router.use(express.static(path.resolve(__dirname, '../../frontend', 'dist')));
+  router.use(express.static(path.resolve("../frontend/dist")));
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
-  router.get(/^(?!\/api).*/, (req, res) => {
+  router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
     );
   });
 }
 
-
-
-
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== 'production') {
+  router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+      'XSRF-Token': csrfToken
+    });
+  });
+}
 
 module.exports = router;
